5f6926bf95d4729dd14d3f576185b466badc6e6d
eyargholder <eyargholder@gmail.com>
2022-09-24 20:11:46 -0500

    Update index.js

--- index.js
+++ index.js
@@ -90,6 +90,22 @@ console.log("The 1/3 threshold key structure" +thresholdKey);
      const transaction = new TransferTransaction()
      .addHbarTransfer(senderAccount, Hbar.fromTinybars(-1))
      .addHbarTransfer(recipientAccount, Hbar.fromTinybars(1));
+    
+    //Schedule a transaction
+    const scheduleTransaction = await new ScheduleCreateTransaction()
+     .setScheduledTransaction(transaction)
+     .execute(client);
+
+    //Get the receipt of the transaction
+    const receipt = await scheduleTransaction.getReceipt(client);
+     
+    //Get the schedule ID
+    const scheduleId = receipt.scheduleId;
+    console.log("The schedule ID is " +scheduleId);
+
+    //Get the scheduled transaction ID
+    const scheduledTxId = receipt.scheduledTransactionId;
+    console.log("The scheduled transaction ID is " +scheduledTxId);
      
 
 
