2a76c1f75991be8f04b6586fbc0589d29cf48507
eyargholder <eyargholder@gmail.com>
2022-09-24 21:09:57 -0500

    Update index.js

--- index.js
+++ index.js
@@ -92,6 +92,20 @@ console.log("The 1/3 threshold key structure" +thresholdKey);
      .addHbarTransfer(senderAccount, Hbar.fromTinybars(-1))
      .addHbarTransfer(recipientAccount, Hbar.fromTinybars(1));
     
+    //Create a schedule transaction
+    const transaction = new ScheduleCreateTransaction()
+     .setScheduledTransaction(transactionToSchedule);
+
+    //Sign with the client operator key and submit the transaction to a Hedera network
+    const txResponse = await transaction.execute(client);
+
+    //Request the receipt of the transaction
+    const receipt = await txResponse.getReceipt(client);
+
+    //Get the schedule ID
+    const scheduleId = receipt.scheduleId;
+    console.log("The schedule ID of the schedule transaction is " +scheduleId);
+    
     //Schedule a transaction
     const scheduleTransaction = await new ScheduleCreateTransaction()
      .setScheduledTransaction(transaction)
